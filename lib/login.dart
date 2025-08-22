78ec36bd47dedda49093c254cd866829aa5c866a
Kefayet Ullah <kefayet107@gmail.com>
2025-08-22 17:13:54 +0600

    Initial Commit

--- lib/login.dart
+++ lib/login.dart
@@ -7,13 +7,25 @@ class Login extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Login App"), centerTitle: true),
-      body: Container(
-        margin: EdgeInsets.all(10.0),
-        color: Colors.teal,
-        height: 300,
-        width: 200,
-        child: Center(
-          child: Text("This is Container", style: TextStyle(fontSize: 20)),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          padding: EdgeInsets.all(25),
+          margin: EdgeInsets.symmetric(horizontal: 25),
+          //color: Colors.teal,
+          height: 300,
+          width: 200,
+          //child: Text("Container", style: TextStyle(fontSize: 20)),
+          decoration: BoxDecoration(
+            color: Colors.blue,
+            // borderRadius: BorderRadius.only(
+            //   topLeft: Radius.circular(50),
+            //   bottomRight: Radius.circular(50),
+            // ),
+            // border: Border.all(color:Colors.green, width:10)
+            //borderRadius: BorderRadius.horizontal(left: Radius.circular(10)),
+            //shape: BoxShape.circle,
+          ),
         ),
       ),
     );
